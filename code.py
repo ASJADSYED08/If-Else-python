31d79bbcebd3ce0491916bfd449534203b117ef2
ASJADSYED08 <hr49335@gmail.com>
2021-10-14 01:00:24 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,32 @@
+"""var1=12
+var2=7
+var3=int(input())
+if var3>var1:
+    print("greater")
+elif var3==var1:
+    print("equal")
+else:
+    print("lesser")
+  """ 
+"""     
+list1=[5,4,6]
+if 1 in list1:
+    print("yes it is in the list")
+else:
+     print("no it is not in the list")"""
+
+"""list5=[1,3,5]
+print(3 in list5)
+if 3 in list5:
+    print("yes it is in list")  """ 
+
+print("ENTER YOUR AGE")
+age=int(input())
+list6=[1,2,3,4,5,6,7,8,9]
+if age in list6:
+    if age>=6:
+               print("You can drive car")
+    else:
+        print("You can't dive the car")
+else:
+    print("YOU CAN'T APPLY")
